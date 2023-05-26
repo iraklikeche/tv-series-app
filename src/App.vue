1f06afa59029fd2dc008d80d54f0e2e2982d2573
iraklikeche <irakli.keche@yahoo.com>
2023-05-26 21:21:23 +0400

    BreakingBad Component

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BreakingBadCard from "./components/BreakingBadCard.vue";
+</script>
+
+<template>
+  <main>
+    <h1>HERO</h1>
+    <BreakingBadCard />
+  </main>
+</template>
+
+<style scoped></style>
